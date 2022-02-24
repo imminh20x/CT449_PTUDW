4d7966b678b60436578666fab957f76fd29dc6e7
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-02-24 16:01:35 +0700

    Cai dat xu ly loi

--- app/controller/contact.controller.js
+++ app/controller/contact.controller.js
@@ -1,28 +1,40 @@
-//create and save a new contact
-exports.create = async (req, res) => {
-  res.send({ message: "create handle" });
-};
+  //create and save a new contact
+  create = async (req, res) => {
+    res.send({ message: "create handle" });
+  };
+  
+  //Retrieve all contacts of a user from the database.
+  findAll = async (req, res) => {
+    res.send({ message: "findAll handle" });
+  };
+  
+  //Retrieve all favorite contacts 
+  findAllFavorite = async (req, res) => {
+    res.send({ message: "findAllFavorite handle" });
+  };
+  
+  //Find a single contact with a id
+  findOne = async (req, res) => {
+    res.send({ message: "findOne handle" });
+  };
+  
+  //Update a contact by a id in the request
+  update = async (req, res) => {
+    res.send({ message: "update handle" });
+  };
+  
+  // Delete a contact with the specified id in the request
+  deleteOne = async (req, res) => {
+    res.send({ message: "delete handle" });
+  };
+  // Delete all contacts of a user from the database
+  deleteAll = async (req, res) => {
+    res.send({ message: "deleteAll handle" });
+  };
+  
+module.exports = {create, findAll, findOne, update, deleteAll, deleteOne}
 
-//Retrieve all contacts of a user from the database.
-exports.findAll = async (req, res) => {
-  res.send({ message: "findAll handle" });
-};
-
-//Find a single contact with a id
-exports.findOne = async (req, res) => {
-  res.send({ message: "findOne handle" });
-};
-
-//Update a contact by a id in the request
-exports.update = async (req, res) => {
-  res.send({ message: "update handle" });
-};
-
-// Delete a contact with the specified id in the request
-exports.delete = async (req, res) => {
-  res.send({ message: "delete handle" });
-};
-// Delete all contacts of a user from the database
-exports.deleteAll = async (req, res) => {
-  res.send({ message: "deleteAll handle" });
-};
+//cach 2 dung class
+// module.exports = class ContactController{
+//   ghi phan nd vao
+// }
--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -1,29 +1,28 @@
 const express = require("express");
-const contacts = require("../controller/contact.controller");
+const {create, findAll, findOne, update, deleteAll, deleteOne} = require("../controller/contact.controller");
 
-module.exports = app => {
+module.exports = (app) => {
   const router = express.Router();
-
   //create a new contact
-  router.post("/", contacts.create);
+  router.post("/", create);
 
   //retrieve all contacts
-  router.get("/", contacts.findAll);
+  router.get("/", findAll);
 
-  //retrieve all favorite contacts
-  router.get("/favorite", contacts.findAllFavorite);
+  // //retrieve all favorite contacts
+  router.get("/favorite", findAllFavorite);
 
-  //retrieve a single contact with id
-  router.get("/:id", contacts.findOne);
+  // // //retrieve a single contact with id
+  router.get("/:id", findOne);
 
-  //update a contact with id
-  router.put("/:id", contacts.update);
+  // // //update a contact with id
+  router.put("/:id", update);
 
-  //delete a contact with id
-  router.delete("/:id", contacts.delete);
+  // // //delete a contact with id
+  router.delete("/:id", deleteOne);
 
-  //delete all contacts
-  router.delete("/", contacts.deleteAll);
+  // // //delete all contacts
+  router.delete("/", deleteAll);
 
   app.use("/api/contacts", router);
 };
