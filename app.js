a84da8a24366aa93c37188493c71f8e739e01eb6
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-02-24 12:20:58 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+//parse requests of content-type - application/json
+app.use(express.json());
+
+//parse requests of content-type - application/x-www-form-urlcoded
+app.use(express.urlencoded({ extended: true }));
+
+//simple route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to contact book application.",
+  });
+});
+
+module.exports = app;
